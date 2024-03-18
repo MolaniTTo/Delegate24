d1e7b1566b9e0578492758f0714f24626d3961d5
arnauitb <arnau.molano.7e7@itb.cat>
2024-03-18 12:39:40 +0000

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate24
+{
+    public class Helper
+    {
+        public static bool IsNatural(int x)
+        {
+            return x > 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Delegate24
+{
+    public class Program
+    {
+        public delegate bool MyDelegate(int x);
+        public static void Main()
+        {
+            MyDelegate myDelegate = new MyDelegate(Helper.IsNatural);
+
+            Console.WriteLine(myDelegate(0));
+        }
+    }
+}
